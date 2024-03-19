4b64cd00f3b8d37b898c4e7af83f1833ea4545b7
Chneemann <Andre-Ke@gmx.de>
2024-03-19 17:35:11 +0100

    design add user dialog

--- src/app/dialog/add-user/add-user.component.ts
+++ src/app/dialog/add-user/add-user.component.ts
@@ -5,14 +5,25 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { DialogData, UserComponent } from '../../user/user.component';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.scss',
+  providers: [provideNativeDateAdapter()],
 })
 export class AddUserComponent {
   constructor(
