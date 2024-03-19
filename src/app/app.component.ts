5b4ea9b17ab259a5afa38bbc25fb018fc62df947
Chneemann <Andre-Ke@gmx.de>
2024-03-19 18:47:18 +0100

    Revert "link firebase"
    This reverts commit 6ac074b9ee376b0268be2be2c0e5da46733a3919.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs/internal/Observable';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -20,15 +18,6 @@ import { Observable } from 'rxjs/internal/Observable';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  firestore: Firestore = inject(Firestore);
-  items$: Observable<any[]>;
-
   title = 'simple_crm';
   showFiller = false;
-
-  constructor() {
-    const aCollection = collection(this.firestore, 'items');
-    this.items$ = collectionData(aCollection);
-    console.log(aCollection);
-  }
 }
