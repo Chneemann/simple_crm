6ac074b9ee376b0268be2be2c0e5da46733a3919
Chneemann <Andre-Ke@gmx.de>
2024-03-19 18:40:21 +0100

    link firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs/internal/Observable';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,6 +20,15 @@ import { RouterModule } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+
   title = 'simple_crm';
   showFiller = false;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items');
+    this.items$ = collectionData(aCollection);
+    console.log(aCollection);
+  }
 }
